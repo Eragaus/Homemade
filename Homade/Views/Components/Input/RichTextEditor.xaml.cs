cd9c5ba1e5363f1ae20f842fa957fb728fef0cd8
Bryan Jougleux <bryan.joug@gmail.com>
2025-11-20 15:03:25 +0100

    :sparkles: Add new note page with rich editor

---
+++ Homade/Views/Components/Input/RichTextEditor.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace Homade.Views.Components.Input;
+
+public partial class RichTextEditor : UserControl
+{
+    public RichTextEditor()
+    {
+        InitializeComponent();
+    }
+    
+    // Extract XAML-formatted content
+    public string GetContent()
+    {
+        var range = new TextRange(Editor.Document.ContentStart, Editor.Document.ContentEnd);
+
+        using (var ms = new MemoryStream())
+        {
+            range.Save(ms, DataFormats.Xaml);
+            return Encoding.UTF8.GetString(ms.ToArray());
+        }
+    }
+
+    // Load XAML-formatted content
+    public void SetContent(string xaml)
+    {
+        if (string.IsNullOrEmpty(xaml))
+        {
+            Editor.Document.Blocks.Clear();
+            return;
+        }
+
+        var range = new TextRange(Editor.Document.ContentStart, Editor.Document.ContentEnd);
+        using var ms = new MemoryStream(Encoding.UTF8.GetBytes(xaml));
+        range.Load(ms, DataFormats.Xaml);
+    }
+}
---
+++ Homade/Views/Note/AddNotePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Homade.Views.Note;
+
+public partial class AddNotePage : Page
+{
+    public AddNotePage()
+    {
+        InitializeComponent();
+    }
+    
+    private void SaveNote_Click(object sender, RoutedEventArgs e)
+    {
+        string title = TitleBox.Text;
+        string content = NoteEditor.GetContent(); // <-- from your component
+
+        // TODO: persist (database, json, file, etc)
+        MessageBox.Show("Note saved:\n" +
+                        "Title: " + title + "\n" +
+                        "Content length: " + content.Length);
+    }
+}
